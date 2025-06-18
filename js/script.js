782dab638ad75d60f5ba91ee201e9328d566010e
mahmudul35 <mahmudul.cse.pust@gmail.com>
2025-06-18 18:17:03 +0600

    added nav,hero slider and programms card(partially)

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+// $("#menu-toggle").click(function () {
+//   console.log("dfjh");
+//   $("#mobileMenu").addClass("active");
+//   $("#close-menu").click(function () {
+//     $("#mobileMenu").removeClass("active");
+//   });
+// });
+
+// const icon = $("#menu-icon");
+// $("#btn").click(function () {
+//   alert("dfds");
+// });
